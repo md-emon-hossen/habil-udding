84dc5d62debf3b50a5ef790e358f673c6fce7ec2
Md Emon Hossen <mdemong87@gmail.com>
2022-11-27 13:28:41 +0600

    navber update

--- componnent/Navber.js
+++ componnent/Navber.js
@@ -1,29 +1,26 @@
-import styles from "../styles/Navber.module.css";
 import Link from "next/link";
-import { useState } from "react"
-import {ImCross} from "react-icons/im";
-import {FaBars} from "react-icons/fa";
+import { useState } from "react";
+import { FaBars } from "react-icons/fa";
+import { ImCross } from "react-icons/im";
+import styles from "../styles/Navber.module.css";
 
-export default function Navber(){
+export default function Navber() {
 
-  const[nav,setNav]=useState(false);
+  const [nav, setNav] = useState(false);
+  console.log(nav);
 
-   return <nav className={styles.navberWrp}>
-            <div className={styles.moboNav}>
-            {nav ? <ImCross className={styles.moboIcons} onClick={() => setNav(!nav)} /> : <FaBars className={styles.moboIcons} onClick={() => setNav(!nav)} />}
-           
-            </div>  
-           
-              <ol className={`${styles.NavItem} ${nav && styles.active}`}>
-                <Link className={styles.link} href='/'><li><a>Home</a></li></Link>
-                <Link className={styles.link} href='/notice'><li><a>Notice</a></li></Link>
-                <Link className={styles.link} href='/admission'><li><a>Admission</a></li></Link>
-                <Link className={styles.link} href='/admistration'><li><a>Administration</a></li></Link>
-
-                <Link className={styles.link} href=''><li><a>Contact us</a></li></Link>
-                <Link className={styles.link} href='/login'><li><a>Login</a></li></Link>
-
-            </ol>
-        </nav>
+  return <nav className={styles.navberWrp}>
+    <div className={styles.moboNav}>
+      {nav ? <ImCross className={styles.moboIcons} onClick={() => setNav(!nav)} /> : <FaBars className={styles.moboIcons} onClick={() => setNav(!nav)} />}
+    </div>
+    <div className={`${styles.NavItem} ${!nav && styles.active}`}>
+      <Link className={styles.link} href='/'>Home</Link>
+      <Link className={styles.link} href='/notice'>Notice</Link>
+      <Link className={styles.link} href='/admission'>Admission</Link>
+      <Link className={styles.link} href='/admistration'>Administration</Link>
+      <Link className={styles.link} href='/'>Contact us</Link>
+      <Link className={styles.link} href='/login'>Login</Link>
+    </div>
+  </nav>
 
 }
