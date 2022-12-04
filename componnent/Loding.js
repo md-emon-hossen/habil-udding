125166ec58ae65d9466f96d0ab35c091207a7492
Md Emon Hossen <mdemong87@gmail.com>
2022-12-04 11:58:55 +0600

    check

--- componnent/Loding.js
+++ componnent/Loding.js
@@ -1,16 +1,17 @@
-import styles from "../styles/Loding.module.css";
+// import styles from "../styles/Loding.module.css";
 
 export default function Loding() {
     return (
         <div className={styles.loadingWrp}>
-            <div className={styles.loader}>
+            {/* <div className={styles.loader}>
                 <div className={styles.wrpDiv}></div>
                 <div className={styles.book__pg}></div>
                 <div className={`${styles.book__pg}${styles.two}`}></div>
                 <div className={`${styles.book__pg}${styles.three}`}></div>
                 <div className={`${styles.book__pg}${styles.four}`}></div >
                 <div className={`${styles.book__pg}${styles.five}`}></div>
-            </div >
+            </div > */}
+            Loding.....
         </div >
     )
 }
