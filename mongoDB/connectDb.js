25eca581a83a1e1a4eec4134db07a28ab6e7e4b9
Md Emon Hossen <mdemong87@gmail.com>
2022-12-11 22:06:05 +0600

    nextEnvUpdate

--- mongoDB/connectDb.js
+++ mongoDB/connectDb.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 async function ConnectDB() {
 
     //connect database
-    await mongoose.connect(`${process.env.DATABASE_URL}`, (err) => {
+    await mongoose.connect(`${process.env.NEXT_PUBLIC_DATABASE_URL}`, (err) => {
         if (!err) {
             console.log("database is connected...");
         } else {
