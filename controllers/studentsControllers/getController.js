c75d5903ce1856654851fcb7f845d9f7656c318e
Md Emon Hossen <mdemong87@gmail.com>
2022-12-11 22:46:42 +0600

    dev1

--- controllers/studentsControllers/getController.js
+++ controllers/studentsControllers/getController.js
@@ -1,12 +1,19 @@
-import { Student } from "../../mongoDB/models/models";
+// import { Student } from "../../mongoDB/models/models";
 
 export default async function getController(req, res) {
 
-    await Student.find((err, data) => {
-        if (!err) {
-            res.status(200).json(data);
-        } else {
-            res.status(500).json({ error: "There was a server side problem" });
-        }
-    });
+
+    console.log(req);
+    res.status(200).json({ check: "ok" });
+
+
+
+
+    // await Student.find((err, data) => {
+    //     if (!err) {
+    //         res.status(200).json(data);
+    //     } else {
+    //         res.status(500).json({ error: "There was a server side problem" });
+    //     }
+    // });
 }
